59bcf2b3ed57c980acad379eb6a6f6beef3726ef
Christian Londoño Cañas <clondo46@eafit.edu.co>
2017-04-20 11:26:27 -1200

    Prueba

--- messages/index.js
+++ messages/index.js
@@ -33,14 +33,12 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
 
-var time = new Date();
-var hora = time.getUTCHours;
-var uno = 1;
 bot.dialog('/', intents);
 intents.matches('Saludos', [
 
     function (session){
         var numero = Math.floor(Math.random() * (10 - 1)) + 1;
+        var time = new Date();
         var hora = time.getHours;
         if(hora > 12){
     session.send('Buenos Dias')
